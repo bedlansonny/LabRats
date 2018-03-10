16f5601a8170f022bf0b84a589904d6d235a8689
bedlansonny <bedlansonny@gmail.com>
2018-03-09 22:52:38 -0600

    works w first test case

--- src/Graph.java
+++ src/Graph.java
@@ -25,18 +25,65 @@ public class Graph
     //https://youtu.be/GazC3A4OQTE
     public int pathWeight(int start, int end)
     {
-        HashSet<Node> visited = new HashSet<>();
-        PriorityQueue<Node> toCheck = new PriorityQueue<>();
-        //each node should have: Node prev and int traveled
+        Node source = getNode(start);
+        Node exit = getNode(end);
+
+        source.traveled = 0;
+
+        PriorityQueue<Node> toCheck = new PriorityQueue<>(); //order to check children
+        toCheck.add(source);
+        Node current;
+        while(toCheck.size() > 0)
+        {
+            current = toCheck.poll();
+            for(Node node : current.adjacent.keySet())
+            {
+                if(!current.visited.contains(node) && node.traveled > current.traveled + current.adjacent.get(node))
+                {
+                    current.visited.add(node);
+                    node.visited.add(current);
+
+                    node.prev = current;
+                    node.traveled = current.traveled + current.adjacent.get(node);
+                    toCheck.add(node);
+                }
+                if(current.id == exit.id)
+                    break;
+            }
+        }
+        if(exit.traveled == Integer.MAX_VALUE)
+            return -1;
+        return exit.traveled;
+
     }
 
-    static class Node
+    public void resetVals()
+    {
+        for(Map.Entry<Integer, Node> entry : nodeLookup.entrySet())
+        {
+            entry.getValue().prev = null;
+            entry.getValue().traveled = Integer.MAX_VALUE;
+            entry.getValue().visited = new HashSet<>();
+        }
+    }
+
+    static class Node implements Comparable<Node>
     {
         int id;
         HashMap<Node, Integer> adjacent = new HashMap<>();  //<node, edge weight>
+
+        Node prev;
+        int traveled = Integer.MAX_VALUE;
+        HashSet<Node> visited = new HashSet<>();
+
         private Node(int id)
         {
             this.id = id;
+        }
+
+        public int compareTo(Node otherNode)
+        {
+            return this.traveled - otherNode.traveled;
         }
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+    public static void main(String args[]) throws IOException
+    {
+        Scanner in = new Scanner(new File("input.txt"));
+        int c = in.nextInt(); //number of test cases
+        for(int ci = 0; ci < c; ci++)
+        {
+            int n = in.nextInt(); //number of cells in the maze
+            int e = in.nextInt(); //exit cell number
+            int t = in.nextInt(); //time duration
+            int m = in.nextInt(); //number of connections in the maze
+            Graph maze = new Graph();
+
+            for(int mi = 0; mi < m; mi++)
+            {
+                int a = in.nextInt();
+                int b = in.nextInt();
+                int weight = in.nextInt();
+
+                maze.addEdge(a, b, weight);
+            }
+
+            int escapers = 0;
+            for(int ni = 1; ni <= n; ni++)
+            {
+                maze.resetVals();
+                int time = maze.pathWeight(ni, e);
+                if(time <= t && time != -1)
+                    escapers++;
+                System.out.printf("cell %d: %d seconds%n", ni, time);
+            }
+
+            System.out.println(escapers);
+        }
+    }
+}
